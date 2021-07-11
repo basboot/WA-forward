5c12e18227f561b863cb33ae9169221a1baa2376
Bas Boot <b.boot@student.tudelft.nl>
2021-07-11 11:02:46 +0200

    Cleanup code

--- index.ts
+++ index.ts
@@ -1,8 +1,5 @@
-// const wa = require('../dist/index');
-// var create = require("@open-wa/wa-automate").create;
-// import { create, Client, decryptMedia, ev } from '../dist/index';
-
-import {Config} from './config';
+// Import class with configuration parameters
+import { Config } from './config';
 
 import { create, Client, decryptMedia, ev, smartUserAgent, NotificationLanguage, MessageTypes, ChatMuteDuration } from '../src/index';
 import { CLIENT_RENEG_WINDOW } from 'tls';
@@ -11,125 +8,104 @@ const fs = require('fs');
 const uaOverride = 'WhatsApp/2.16.352 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Safari/605.1.15';
 const tosBlockGuaranteed = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/79.0.3945.88 Safari/537.36";
 const ON_DEATH = require('death');
-let globalClient:Client;
+let globalClient: Client;
 const express = require('express')
 
 const app = express()
-app.use(express.json({limit: '200mb'})) //add the limit option so we can send base64 data through the api
+app.use(express.json({ limit: '200mb' })) //add the limit option so we can send base64 data through the api
 
 const PORT = 8082;
 
 
-ON_DEATH(async function(signal, err) {
+ON_DEATH(async function (signal, err) {
   console.log('killing session');
-  if(globalClient)await globalClient.kill();
+  if (globalClient) await globalClient.kill();
 })
 
 /**
  * Detect the qr code
  */
-ev.on('qr.**', async (qrcode,sessionId) => {
+ev.on('qr.**', async (qrcode, sessionId) => {
   //base64 encoded qr code image
-  const imageBuffer = Buffer.from(qrcode.replace('data:image/png;base64,',''), 'base64');
-  fs.writeFileSync(`qr_code${sessionId?'_'+sessionId:''}.png`, imageBuffer);
+  const imageBuffer = Buffer.from(qrcode.replace('data:image/png;base64,', ''), 'base64');
+  fs.writeFileSync(`qr_code${sessionId ? '_' + sessionId : ''}.png`, imageBuffer);
 });
 
 /**
  * Detect when a session has been started successfully
  */
-ev.on('STARTUP.**', async (data,sessionId) => {
-  if(data==='SUCCESS') console.log(`${sessionId} started!`)
+ev.on('STARTUP.**', async (data, sessionId) => {
+  if (data === 'SUCCESS') console.log(`${sessionId} started!`)
 })
 
-// /**
-//  * Detect all events
-//  */
-// ev.on('**', async (data,sessionId,namespace) => {
-//   console.log("\n----------")
-//   console.log('EV',data,sessionId,namespace)
-//   console.log("----------")
-// })
+/**
+ * Detect all events
+ */
+ev.on('**', async (data,sessionId,namespace) => {
+  console.log("\n----------")
+  console.log('EV',data,sessionId,namespace)
+  console.log("----------")
+})
 
-// /**
-//  * Detect the session data object
-//  */
-// ev.on('sessionData.**', async (sessionData, sessionId) =>{
-//   console.log("\n----------")
-//   console.log('sessionData',sessionId, sessionData)
-//   console.log("----------")
-// })
+/**
+ * Detect the session data object
+ */
+ev.on('sessionData.**', async (sessionData, sessionId) =>{
+  console.log("\n----------")
+  console.log('sessionData',sessionId, sessionData)
+  console.log("----------")
+})
 
-// /**
-//  * Detect the session data object encoded as a base64string
-//  */
-// ev.on('sessionDataBase64.**', async (sessionData, sessionId) =>{
-//   console.log("\n----------")
-//   console.log('sessionData',sessionId, sessionData)
-//   console.log("----------")
-// })
+/**
+ * Detect the session data object encoded as a base64string
+ */
+ev.on('sessionDataBase64.**', async (sessionData, sessionId) =>{
+  console.log("\n----------")
+  console.log('sessionData',sessionId, sessionData)
+  console.log("----------")
+})
 
 async function start(client: Client) {
   app.use(client.middleware(true));
 
-app.listen(PORT, function () {
-  console.log(`\n• Listening on port ${PORT}!`);
-});
+  app.listen(PORT, function () {
+    console.log(`\n• Listening on port ${PORT}!`);
+  });
 
-  globalClient=client;
-  console.log(`Starting WA-forwarder for: ${Config.remote_phone_number}`)
+  globalClient = client;
+  console.log(`Starting WA-forwarder for: ${Config.remotePhoneNumber}`)
   const me = await client.getMe();
   console.log("start -> me", me);
-  // const chats = await client.getAllChatsWithMessages(false);
-  // console.log("TCL: start -> chats", chats)
-  // console.log("TCL: getAllChatsWithMessages ->", chats.length, chats[0]);
-  // console.log("TCL: start ->chats", chats[0].msgs);
 
-  // const newMessages = await client.getAllUnreadMessages();
-  // console.log("TCL: start -> newMessages", newMessages)
-  // console.log("TCL: getAllNewMessages ->", newMessages.length, newMessages[0]);
+  client.onAck((c: any) => console.log(c.id, c.body, c.ack));
+  client.onAddedToGroup(newGroup => console.log('Added to new Group', newGroup.id));
+  client.onIncomingCall(call => console.log('newcall', call));
 
-  client.onAck((c:any) => console.log(c.id,c.body,c.ack));
+  const prods = await client.getBusinessProfilesProducts(me.wid)
+  console.log(prods)
 
-    client.onAddedToGroup(newGroup => console.log('Added to new Group', newGroup.id));
-
-    client.onIncomingCall(call=>console.log('newcall',call));
-
-
-    const prods = await client.getBusinessProfilesProducts(me.wid)
-    console.log(prods)
-
-
-  // client.onParticipantsChanged("XXXXXXXX-YYYYYYYY@g.us", (participantChangedEvent:any) => console.log("participant changed for group", participantChangedEvent));
-  
-  //Returns 'CONNECTED' or 'TIMEOUT' or 'CONFLICT' (if user opens whatsapp web somewhere else)
-  client.onStateChanged(state=>{
+  client.onStateChanged(state => {
     console.log('statechanged', state)
-    if(state==="CONFLICT" || state==="UNLAUNCHED") client.forceRefocus();
+    if (state === "CONFLICT" || state === "UNLAUNCHED") client.forceRefocus();
   });
 
-  // setTimeout(_=> client.kill(), 3000);
-
-  // const allmsgs = await client.loadAndGetAllMessagesInChat('XXXXXXXX-YYYYYYYY@g.us",true,false);
-  // console.log("TCL: start -> allMessages", allmsgs.length);
-
-  client.onAnyMessage(message=>{
+  client.onAnyMessage(message => {
     console.log(message.type)
-    // if(message.body==='DELETE') client.deleteMessage(message.from,message.id,false)
   });
-  // client.onParticipantsChanged("XXXXXXXXXX-YYYYYYYYY@g.us",x=>console.log(x))
+
   client.onMessage(async message => {
     console.log("--- NEW MESSAGE ---");
     console.log(message);
 
     try {
       let txtMessage = "";
-      if (message.type == MessageTypes.TEXT ) {
+      if (message.type == MessageTypes.TEXT) {
         txtMessage = message.body;
         if (message.quotedMsg != null) {
           console.log("THIS IS A REPLY");
 
-          
-          if (message.from == `${Config.remote_phone_number}@c.us`) {
+
+          if (message.from == `${Config.remotePhoneNumber}@c.us`) {
             let contactId = message.quotedMsg.content.split('\n')[1];
             console.log(`Relay this message to: ${contactId}`)
           } else {
@@ -145,119 +121,19 @@ app.listen(PORT, function () {
         const filename = `${message.t}.${mime.extension(message.mimetype)}`;
 
         let mediaData = await decryptMedia(message, uaOverride);
-        await client.sendImage(`${Config.remote_phone_number}@c.us`,
-        `data:${message.mimetype};base64,${mediaData.toString('base64')}`,
-        filename,
-        `*${message.sender.formattedName}:* ${txtMessage} (${message.chat.formattedTitle})\n${message.from}`);
+        await client.sendImage(`${Config.remotePhoneNumber}@c.us`,
+          `data:${message.mimetype};base64,${mediaData.toString('base64')}`,
+          filename,
+          `*${message.sender.formattedName}:* ${txtMessage} (${message.chat.formattedTitle})\n${message.from}`);
       } else {
         console.log(">>> Send text");
-        client.sendText(`${Config.remote_phone_number}@c.us`, `*${message.sender.formattedName}:* ${txtMessage} (${message.chat.formattedTitle})\n${message.from}`);
+        client.sendText(`${Config.remotePhoneNumber}@c.us`, `*${message.sender.formattedName}:* ${txtMessage} (${message.chat.formattedTitle})\n${message.from}`);
       }
     } catch (error) {
       console.log("Problem in 'onMessage' -> error", error);
     }
-
-    // try {
-
-    // const mp3_message_id = await client.sendAudio(message.from,'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3', null)
-    // console.log("start -> mp", mp3_message_id)
-
-    // const isConnected = await client.isConnected();
-    // console.log("TCL: start -> isConnected", isConnected)
-    // console.log(message.body, message.id, message?.quotedMsgObj?.id);
-    // if (message.mimetype) {
-    //   const filename = `${message.t}.${mime.extension(message.mimetype)}`;
-
-    //   // if it is a sticker, you need to run this.
-    //   let mediaData;
-    //   if( message.type==='sticker') {
-    //     //getStickerDecryptable is an insiders feature! 
-    //     let stickerDecryptable = await client.getStickerDecryptable(message.id);
-    //     if(stickerDecryptable) mediaData = await decryptMedia(stickerDecryptable, uaOverride);
-    //   } else {
-    //     mediaData = await decryptMedia(message, uaOverride);
-    //   }
-    //   if(message.type==='video') {
-    //       const mp4_as_sticker = await client.sendMp4AsSticker(message.from,mediaData);
-    //       console.log("start -> mp4_as_sticker", mp4_as_sticker)
-    //   }
-    //   // you can send a file also with sendImage or await client.sendFile
-    //   await client.sendImage(
-    //     message.from,
-    //     `data:${message.mimetype};base64,${mediaData.toString('base64')}`,
-    //     filename,
-    //     `You just sent me this ${message.type}`
-    //   );
-      
-    //   //send the whole data URI so the mimetype can be checked.
-    //   await client.sendImageAsSticker(message.from, `data:${message.mimetype};base64,${mediaData.toString('base64')}`)
-    //   //get this numbers products
-    //   // const products = await client.getBusinessProfilesProducts(message.to);
-
-    //   // //send a product from this number to that number
-    //   //  await client.sendImageWithProduct(
-    //   //   `data:${message.mimetype};base64,${mediaData.toString('base64')}`,
-    //   //   message.from,
-    //   //   'check out this product',
-    //   //   message.to,
-    //   //   products[0].id)
-
-    //     // await client.forwardMessages(message.from,message,false);
-
-    //     await client.forwardMessages(message.from,message.id,false);
-    //   fs.writeFileSync(filename, mediaData, function(err) {
-    //     if (err) {
-    //       return console.log(err);
-    //     }
-    //     console.log('The file was saved!');
-    //   });
-
-    //   /**
-    //    * You can also send the file as a relative file reference. The library will automatically open the file and get the dataUrl
-    //    */
-    //   const message_id_from_file = await client.sendImage(message.from,
-    //     './'+filename,
-    //     filename,
-    //     'from file',
-    //     null,
-    //     true,
-    //     false
-    //     )
-    //   console.log("start -> message_id", message_id_from_file)
-
-    //   /**
-    //    * Now you can send an animated gif via url
-    //    */
-    //   const sticker_from_url_gif_id = await client.sendStickerfromUrl(message.from, "https://i.giphy.com/media/yJil9u57ybQ9movc6E/source.gif")
-    //   console.log("start -> sticker_from_url_gif_id", sticker_from_url_gif_id)
-
-    // } else if (message.type==="location") {
-    //   if(message.shareDuration) console.log('This user has started sharing their live location', message.author || message.from)
-    //   console.log("TCL: location -> message", message.lat, message.lng, message.loc)
-    //   await client.sendLocation(message.from, `${message.lat}`, `${message.lng}`, `Youre are at ${message.loc}`)
-    // } else {
-    //   // var sentMessageId = await client.sendText(message.from, message.body);
-    //   // console.log("start -> sentMessageId", sentMessageId)
-    //   // //send a giphy gif
-    //   //   await client.forwardMessages(message.from,message,false);
-    //   // await client.sendGiphy(message.from,'https://media.giphy.com/media/oYtVHSxngR3lC/giphy.gif','Oh my god it works');
-    //   // console.log("TCL: start -> message.from,message.body,message.id.toString()", message.from,message.body,message.id.toString())
-    //   // await client.reply(message.from,message.body,message);
-    // }
-    // } catch (error) {
-    // console.log("TCL: start -> error", error)
-    // }
   });
-
-    // const groupCreationEvent = await client.createGroup('coolnewgroup','0000000000@c.us');
-    // console.log("start -> groupCreationEvent", groupCreationEvent)
-  //wait a few seconds and make a group
-
 }
-
-//you can now create two sessions pointing 
-//two the same message handler
-
 
 /**
  * it can be null, which will default to 'session' folder.
@@ -265,16 +141,15 @@ app.listen(PORT, function () {
  * and you can AND SHOULD override the user agent.
  */
 create({
-  sessionId:'WA-forwarder',
-  // executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+  sessionId: 'WA-forwarder',
   useChrome: true,
   restartOnCrash: start,
-  headless:false,
-  throwErrorOnTosBlock:true,
-  qrTimeout:0,   //set to 0 to wait forever for a qr scan
-  authTimeout:0, //set to 0 to wait forever for connection to phone
+  headless: false,
+  throwErrorOnTosBlock: true,
+  qrTimeout: 0,   //set to 0 to wait forever for a qr scan
+  authTimeout: 0, //set to 0 to wait forever for connection to phone
   killProcessOnBrowserClose: true,
-  autoRefresh:true, //default to true
+  autoRefresh: true, //default to true
   safeMode: true,
   disableSpins: true,
   hostNotificationLang: NotificationLanguage.PTBR,
@@ -300,17 +175,10 @@ create({
   //   '--disk-cache-size=0'
   // ]
 })
-// create()
-.then(async client => await start(client))
-.catch(e=>{
-  console.log('Error',e.message);
-  // process.exit();
-});
+  // create()
+  .then(async client => await start(client))
+  .catch(e => {
+    console.log('Error', e.message);
+    // process.exit();
+  });
 
-//or you can set a 'session id'
-// create('newsession').then(client => start(client));
-
-//DO NOT HAVE TO SESSIONS WITH THE SAME ID
-
-//BE WARNED, SETTING THIS UP WITH 2 NUMBERS WILL RESULT IN AN ECHO CHAMBER
-//IF YOU SEND AN IMAGE WITH ONE PHONE IT WILL PING PONG THAT IMAGE FOR ETERNITY
