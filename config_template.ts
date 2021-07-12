676359f3c28b5f2bb6d0a7650f1d2eb0efe54dd4
Bas Boot <b.boot@student.tudelft.nl>
2021-07-12 17:08:22 +0200

    Created template for config.ts

---
+++ config_template.ts
@@ -0,0 +1,10 @@
+// Set correct phone number
+// Save file as 'config.ts'
+
+// Static class with configuration parameters for WA-forward
+
+export class Config {
+    // phone number including country code (eg. 31 for NL), but without a +
+    public static remotePhoneNumber: number = 31000000000; 
+}
+
