6fd127c50d4a64bce7dfb0a002c5d2536d41f3be
Bas Boot <b.boot@student.tudelft.nl>
2021-07-12 17:05:00 +0200

    Delete config.ts

--- config.ts
+++
@@ -1,6 +0,0 @@
-// Static class with configuration parameters for WA-forward
-
-export class Config {
-    // phone number of remote phone including country code (eg. 31 for NL), but without a +
-    public static remotePhoneNumber: number = 31000000000; 
-}
