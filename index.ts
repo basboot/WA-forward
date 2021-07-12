54a56a8d6052360ea85cdbf98d60edea1c4a2df2
Bas Boot <b.boot@student.tudelft.nl>
2021-07-12 13:35:44 +0200

    Added forwarder state to enable/disable forward, relay and test messages

--- index.ts
+++ index.ts
@@ -18,6 +18,15 @@ app.use(express.json({ limit: '200mb' })) //add the limit option so we can send 
 
 const PORT = 8082;
 
+// Operatiuon mode of the bot
+// forward: forward incoming messages to remote phone
+// relay  : relay incoming messages from the remote phone to the original sender
+// test   : in test mode , messages from remote phone are also forwarded (back to remote phone)
+let ForwarderState = {"forward" : true, "relay": true, "test": true};
+
+function formattedForwarderState() {
+  return`F: ${ForwarderState.forward}, R: ${ForwarderState.relay}, T: ${ForwarderState.test}`;
+}
 
 ON_DEATH(async function (signal, err) {
   Debug.log(Debug.DEBUG, 'killing session');
@@ -79,6 +88,9 @@ async function start(client: Client) {
   const me = await client.getMe();
   Debug.log(Debug.INFORMATION, "start -> me", me);
 
+  // inform remote phone the script is active
+  client.sendText(`${Config.remotePhoneNumber}@c.us`, `*Forwarder started*\n${formattedForwarderState()}`);
+
   client.onAck((c: any) => Debug.log(Debug.VERBOSE, c.id, c.body, c.ack));
   client.onAddedToGroup(newGroup => Debug.log(Debug.VERBOSE, 'Added to new Group', newGroup.id));
   client.onIncomingCall(call => Debug.log(Debug.VERBOSE, 'newcall', call));
@@ -128,25 +140,51 @@ async function start(client: Client) {
             let contactNumber:number = parseInt(contactId.split('@')[0]);
             let contactType = contactId.split('@')[1];
             
-            // send message
-            if (contactType == "c.us") {
-              client.sendText(`${contactNumber}@c.us`, `${txtMessage}`);
+            if (ForwarderState.relay) {
+              Debug.log(Debug.VERBOSE, `Relaying message to ${contactId}`);
+              // send message
+              if (contactType == "c.us") {
+                client.sendText(`${contactNumber}@c.us`, `${txtMessage}`);
+              } else {
+                Debug.log(Debug.ERROR, "Group sending not implemented yet");  
+              }
             } else {
-              Debug.log(Debug.ERROR, "Group sending not implemented yet");  
-            }
+              Debug.log(Debug.VERBOSE, `Received relaying message for ${contactId}, but relaying is disabled`);
+            }            
           } else {
+            // split message into command and parameter
+            let command = message.body.toLowerCase().split(' ')[0];
+            let param = message.body.toLowerCase().split(' ')[1] == "1"; // only true and false
+            let commandResponse = "No response set";
             // process command
-            switch (message.body.toLowerCase()) {
+            switch (command) {
               case "exit":
                 Debug.log(Debug.WARNING, "Exit script on user request")
+                // TODO: also send this message on user interrupt
+                await client.sendText(`${Config.remotePhoneNumber}@c.us`, "*Forwarder exited*");
                 process.exit(0);
                 break;
               case "ping":
-                client.sendText(`${Config.remotePhoneNumber}@c.us`, `Still alive _(${new Date().toLocaleString()})_`);
+                commandResponse = `${formattedForwarderState()} _(still alive)_`;
+                break;
+              case "forward":
+                ForwarderState.forward = param;
+                commandResponse = `${formattedForwarderState()}`;
+                break;
+              case "relay":
+                ForwarderState.relay = param;
+                commandResponse = `${formattedForwarderState()}`;
+                break;
+              case "test":
+                ForwarderState.test = param;
+                commandResponse = `${formattedForwarderState()}`;
                 break;
               default:
                 Debug.log(Debug.ERROR, `Command '${message.body}' unknown`);
+                commandResponse = `Command '${message.body}' unknown`;
             }
+            // send response to remote phone
+            client.sendText(`${Config.remotePhoneNumber}@c.us`, commandResponse);
             
           }
         } else {
@@ -155,8 +193,11 @@ async function start(client: Client) {
       } catch (error) {
         Debug.log(Debug.ERROR, "Problem in 'onMessage' -> error", error);
       }
-    } else {
-      // Message not from me => forward message to other phone
+    } 
+    // Not splitted into if/else, to allow forwarding messages from self for testing
+
+    // Message not from me (or in test mode) => forward message to other phone
+    if ((!messageFromMe) || (ForwarderState.test)) {
       try {  
         // set message text
         if (message.type == MessageTypes.TEXT ) {
@@ -165,19 +206,23 @@ async function start(client: Client) {
           txtMessage = `Received message of type '${message.type}'`;
           // TODO: add caption
         }
-        
-        if (message.type == MessageTypes.IMAGE) {
-          Debug.log(Debug.DEBUG, ">>> Send image");
-          const filename = `${message.t}.${mime.extension(message.mimetype)}`;
-  
-          let mediaData = await decryptMedia(message, uaOverride);
-          await client.sendImage(`${Config.remotePhoneNumber}@c.us`,
-            `data:${message.mimetype};base64,${mediaData.toString('base64')}`,
-            filename,
-            `*${message.sender.formattedName}:* ${txtMessage} (${message.chat.formattedTitle})\n${message.from}`);
+
+        if (ForwarderState.forward) {
+          if (message.type == MessageTypes.IMAGE) {
+            Debug.log(Debug.DEBUG, ">>> Send image");
+            const filename = `${message.t}.${mime.extension(message.mimetype)}`;
+    
+            let mediaData = await decryptMedia(message, uaOverride);
+            await client.sendImage(`${Config.remotePhoneNumber}@c.us`,
+              `data:${message.mimetype};base64,${mediaData.toString('base64')}`,
+              filename,
+              `*${message.sender.formattedName}:* ${txtMessage} (${message.chat.formattedTitle})\n${message.from}`);
+          } else {
+            Debug.log(Debug.DEBUG, ">>> Send text");
+            client.sendText(`${Config.remotePhoneNumber}@c.us`, `*${message.sender.formattedName}:* ${txtMessage} (${message.chat.formattedTitle})\n${message.from}`);
+          }
         } else {
-          Debug.log(Debug.DEBUG, ">>> Send text");
-          client.sendText(`${Config.remotePhoneNumber}@c.us`, `*${message.sender.formattedName}:* ${txtMessage} (${message.chat.formattedTitle})\n${message.from}`);
+          Debug.log(Debug.VERBOSE, `Received message '${txtMessage}' from ${message.sender.formattedName}, but forwarding is disabled`);
         }
       } catch (error) {
         Debug.log(Debug.ERROR, "Problem in 'onMessage' -> error", error);
@@ -232,4 +277,3 @@ create({
     Debug.log(Debug.ERROR, 'Error', e.message);
     // process.exit();
   });
-
